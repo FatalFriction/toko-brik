4847e2c018f83812ec7ad561b4de1526e3fc05dc
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-14 01:34:37 +0700

    feat: reusable types

---
+++ src/types/index.tsx
@@ -0,0 +1,16 @@
+interface ProductTypes {
+    id: number;
+    categoryId: number;
+    categoryName: string;
+    sku: string;
+    name: string;
+    description: string;
+    weight: number;
+    width: number;
+    length: number;
+    height: number;
+    image: string;
+    harga: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
