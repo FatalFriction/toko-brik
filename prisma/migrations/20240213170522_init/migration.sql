c29531523cba56e860410ff9e335f899b268e8cc
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-14 00:56:08 +0700

    init: migration

---
+++ prisma/migrations/20240213170522_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "categoryName" TEXT NOT NULL,
+    "sku" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "weight" INTEGER NOT NULL,
+    "width" INTEGER NOT NULL,
+    "length" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "image" TEXT NOT NULL,
+    "harga" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Product_createdAt_idx" ON "Product"("createdAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_id_key" ON "Product"("id");
