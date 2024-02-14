2f03c7a7d20ed46c72c96fadd842d646b60347f4
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-14 14:58:50 +0700

    feat: signUp form

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import SignUpForm from '@/components/form/SignUpForm'
+import React from 'react'
+
+const signUp = () => {
+  return (
+    <div className='w-[30%] mx-auto mt-60'>
+      <SignUpForm/>
+    </div>
+  )
+}
+
+export default signUp
