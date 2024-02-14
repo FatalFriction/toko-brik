164d1d602a1512ed46404ed467dfdab1e0e07ffd
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-14 10:11:33 +0700

    feat: next auth config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const dotenv = require('dotenv');
+dotenv.config({ path: '../../../../../.env.development' });
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+        })
+    ]
+}
+
+export const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
