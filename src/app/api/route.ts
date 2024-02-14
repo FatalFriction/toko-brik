b50b4d2623a0a43a066dfe9f449f4e871ec2c260
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-14 11:52:33 +0700

    feat: server validation protecting api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,17 @@
 import { NextResponse } from "next/server";
 import db from "@/modules/db";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export async function GET(
   req: Request,
 ) {
   try {
+    const session = await getServerSession();
+
+    if(!session || !session.user) {
+      redirect("/api/auth/signin");
+    }
 
     const products = await db.product.findMany({ orderBy: { createdAt: 'asc' } })
 
